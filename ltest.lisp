3f96adc024ad892c82c4a5b8e9c09f9ce393791c
Lieven <lieven@risolto.be>
2024-08-27 18:01:52 +0200

    cleanup-of-deprecated-code

--- ltest.lisp
+++ ltest.lisp
@@ -49,22 +49,6 @@ Wrapper to optonally write to a specific output file
         (setf result (eval test-suite)))
         result
     ))
-#|
-(defun to-output (&key test-suite output-file)
-    (if output-file
-        (progn
-            (setf *output-file* output-file)
-            (with-open-file (stream *output-file*
-                :direction :output
-                :if-exists :overwrite
-                :if-does-not-exist :create)
-                (let* ((*standard-output* stream)
-                        (result (eval test-suite)))
-                    (progn
-                        (open-output-file output-file)
-                        result))))
-        (eval test-suite)))
- |#
 
 #|
 Helper functions to print out test results
