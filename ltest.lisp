f7be1f9487ef954bec2514c09939a10b164bee12
Lieven <lieven@risolto.be>
2024-08-27 18:44:58 +0200

    clean-formatting

--- ltest.lisp
+++ ltest.lisp
@@ -68,33 +68,43 @@ Helper functions to print out test results
     (if (not *output-file*)
         (format nil "~c[31m~a~c[0m" #\esc str #\esc)
         str))
+
+(defun insert-padding (str1 str2 len pad-char)
+    (let* ((len1 (length str1))
+            (len2 (length str2))
+            (len-padding (max (- len (+ len1 len2)) 0))
+            (padding-str (make-string len-padding :element-type 'character :initial-element pad-char)))
+        (format nil "~a~a~a" str1 padding-str str2)))
  
 (defun pass (f-name var1 var2 &key (predicates nil) (test-name nil)) 
-    (let ((pred-str (if predicates (format nil " ~{~a~^ ~} |" predicates) ""))
-            (test-name-str (if test-name (format nil "~a | " test-name) "")))
-        (format T
-            (green "~%~a~a |~a ~a ~a | ~a ~a => PASS...")
-            test-name-str f-name pred-str
-            (type-of var1) var1 (type-of var2) var2)
+    (let* ((pred-str (if predicates (format nil " ~{~a~^ ~} |" predicates) ""))
+            (test-name-str (if test-name (format nil "~a | " test-name) ""))
+            (result-str (format nil
+                "~%~a~a |~a ~a ~a | ~a ~a "
+                test-name-str f-name pred-str
+                (type-of var1) var1 (type-of var2) var2)))
+        (format T (green (insert-padding result-str " [PASS]" 120 #\.)))
         :pass))
 
 (defun fail (f-name var1 var2 &key (predicates nil) (test-name nil)) 
-    (let ((pred-str (if predicates (format nil " ~{~a~^ ~} |" predicates) ""))
-            (test-name-str (if test-name (format nil "~a | " test-name) "")))
-        (format T
-            (red "~%~a~a |~a ~a ~a | ~a ~a => FAIL!!!")
-            test-name-str f-name pred-str
-            (type-of var1) var1 (type-of var2) var2)
+    (let* ((pred-str (if predicates (format nil " ~{~a~^ ~} |" predicates) ""))
+            (test-name-str (if test-name (format nil "~a | " test-name) ""))
+            (result-str (format nil
+                "~%~a~a |~a ~a ~a | ~a ~a "
+                test-name-str f-name pred-str
+                (type-of var1) var1 (type-of var2) var2)))
+        (format T (red (insert-padding result-str " [FAIL]" 120 #\.)))
         :fail))
     
 
 (defun invalid (f-name var1 var2 &key (predicates nil) (test-name nil)) 
-    (let ((pred-str (if predicates (format nil " ~{~a~^ ~} |" predicates) ""))
-            (test-name-str (if test-name (format nil "~a | " test-name) "")))
-        (format T
-            (yellow "~%~a~a |~a ~a ~a | ~a ~a => INVALID???")
-            test-name-str f-name pred-str
-            (type-of var1) var1 (type-of var2) var2)
+    (let* ((pred-str (if predicates (format nil " ~{~a~^ ~} |" predicates) ""))
+            (test-name-str (if test-name (format nil "~a | " test-name) ""))
+            (result-str (format nil
+                "~%~a~a |~a ~a ~a | ~a ~a "
+                test-name-str f-name pred-str
+                (type-of var1) var1 (type-of var2) var2)))
+        (format T (yellow (insert-padding result-str " [INVALID]" 120 #\.)))
         :invalid))
 
 #|
