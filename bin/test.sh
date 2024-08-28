60fbff88fc7a2f0ad1b4d057b776b5c03acad898
Lieven <lieven@risolto.be>
2024-08-28 14:37:27 +0200

    bash

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash sbcl --script
 
 (load "./src/colour.lisp")
+(load "./src/liter.lisp")
+(load "./src/lcheck.lisp")
+(load "./src/lassert.lisp")
 (load "./src/out.lisp")
 (load "./src/ltest.lisp")
 (load "./test/test.lisp")
