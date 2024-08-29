f3984a6f59530e6a9502756b6ce8d9c3630cb147
lvnboes <lvn.boes@gmail.com>
2024-08-29 02:22:05 +0200

    document-out

--- src/out.lisp
+++ src/out.lisp
@@ -7,6 +7,7 @@
 (defparameter *output-width* 100)
 
 (defun test-result-prefix (test-name result &optional (width *output-width*) (pad-char #\.))
+    "Based on the test name and result, create a prefix for the test report"
     (let* ((name-len (length test-name))
             (result-len (length result))
             (padding-len (- width name-len result-len 2))
@@ -14,6 +15,9 @@
         (format nil "~a ~a ~a~%" test-name padding result)))
 
 (defun test-result-assertions (assertions &optional (assertion-strings '()))
+    "Based on the details of the assertions in the test execution, create a detailed report string 
+        on all the failed and invalid assertions for inclusion in the tests reporting message 
+        and return the assertion report string. Empty if the test passed"
     (if (null assertions) 
         (apply 'concatenate 'string (reverse assertion-strings))
         (progn
@@ -38,6 +42,8 @@
             (test-result-assertions (cdr assertions) assertion-strings))))
 
 (defun test-result-suffix (pass fail invalid &optional (width *output-width*) (pad-char #\Space))
+    "Based on number of passed, failed and invalid assertions in the test execution, create a suffix 
+        string for the reporting message and return the suffix string. Empty if the test passed."
     (let* ((total-str (format nil "Total assertions: ~a " (+ pass fail invalid)))
             (passed-str (format nil "Passed: ~a " pass))
             (failed-str (format nil "Failed: ~a " fail))
@@ -56,6 +62,8 @@
             invalid-str)))
 
 (defun test-result (result-table)
+    "Based on the results of a unit test, create a reporting string 
+        and push to output."
     (let* ((name (gethash :name result-table))
             (result (gethash :result result-table))
             (total (+ (gethash :pass result-table 0) 
