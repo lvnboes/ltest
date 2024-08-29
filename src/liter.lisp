ff298e6651b174f38eba3e12b180aefa06a1fc11
lvnboes <lvn.boes@gmail.com>
2024-08-29 02:08:23 +0200

    document-iter.lisp

--- src/liter.lisp
+++ src/liter.lisp
@@ -6,50 +6,73 @@
 
 (in-package :liter)
 
+#|
+Herlper functions for iterating over predicates provided values
+ |#
+
 (defun all-val (pred-or-preds vals x &optional (fn #'funcall)) 
+    "Check if the provided predicate(s) evaluate(s) to t for 
+        all provided values in relation to the expected value"
     (cond ((null vals) t)
         ((funcall fn pred-or-preds (car vals) x)
             (all-val pred-or-preds (cdr vals) x))
         (t nil)))
 
 (defun no-val (pred-or-preds vals x &optional (fn #'funcall)) 
+    "Check if the provided predicate(s) evaluate(s) to t for 
+        none of the provided values in relation to the expected value"
     (cond ((null vals) t)
         ((funcall fn pred-or-preds (car vals) x) nil)
         (t (no-val pred-or-preds (cdr vals) x))))
 
 (defun some-val (pred-or-preds vals x &optional (fn #'funcall)) 
+    "Check if the provided predicate(s) evaluate(s) to t for 
+        some of the provided values in relation to the expected value"
     (cond ((null vals) nil)
         ((funcall fn pred-or-preds (car vals) x) t)
         (t (some-val pred-or-preds (cdr vals) x))))
 
 (defun some-not-val (pred-or-preds vals x &optional (fn #'funcall)) 
+    "Check if the provided predicate(s) evaluate(s) to nil for 
+        some of the provided values in relation to the expected value"
     (cond ((null vals) nil)
         ((funcall fn pred-or-preds (car vals) x)
             (some-not-val pred-or-preds (cdr vals) x))
         (t t)))
 
 (defun all-pred (preds val x) 
+    "Check if applying the provided predicates to the provided and 
+        expected values evaluates to t for all predicates"
     (cond ((null preds) t)
         ((funcall (car preds) val x)
             (all-pred (cdr preds) val x))
         (t nil)))
 
 (defun no-pred (preds val x) 
+    "Check if applying the provided predicates to the provided and 
+        expected values evaluates to t for none of the predicates"
     (cond ((null preds) t)
         ((funcall (car preds) val x) nil)
         (t (no-pred (cdr preds) val x))))
 
 (defun some-pred (preds val x) 
+    "Check if applying the provided predicates to the provided and 
+        expected values evaluates to t for some of the predicates"
     (cond ((null preds) nil)
         ((funcall (car preds) val x) t)
         (t (some-pred (cdr preds) val x))))
 
 (defun some-not-pred (preds val x) 
+    "Check if applying the provided predicates to the provided and 
+        expected values evaluates to nil for some of the predicates"
     (cond ((null preds) nil)
         ((funcall (car preds) val x) 
             (some-not-pred (cdr preds) val x))
         (t t)))
 
-(defun iter-2 (var-fn pred-fn)
+(defun iter-2 (val-fn pred-fn)
+    "Combine one of the functions for iterating over the provided values
+        and one of the functions for iterating over the predicates
+        and return a combined (anonymous) function."
     #'(lambda (preds vals x)
-        (funcall var-fn preds vals x pred-fn)))
+        (funcall val-fn preds vals x pred-fn)))
