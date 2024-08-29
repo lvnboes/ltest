e383b58c9324b9bf7ee564c2256c95f6c527b5bc
Lieven <lieven@risolto.be>
2024-08-29 16:17:51 +0200

    more-space

--- lib/ltest/out.lisp
+++ lib/ltest/out.lisp
@@ -140,7 +140,7 @@
             (separator (make-string  *output-width* :initial-element #\-))
             (print-colour (get-colour-fun (gethash :result result-table)))
             (result-str (format nil 
-                "~%~a~%~a~%TEST SUITE ~a WITH ~a TEST SETS~%RESULT: ~a~%PASSED: ~a        FAILED: ~a~%~a~%~a~%~%" 
+                "~%~%~a~%~a~%TEST SUITE ~a WITH ~a TEST SETS~%RESULT: ~a~%PASSED: ~a        FAILED: ~a~%~a~%~a~%~%" 
                 separator separator
                 (string-upcase (gethash :name result-table))
                 (+ (gethash :pass result-table) (gethash :fail result-table))
