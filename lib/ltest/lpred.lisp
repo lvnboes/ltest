0d3dbb037011b6d8d81a17cf12623d8845948ce3
lvnboes <lvn.boes@gmail.com>
2024-08-31 00:22:16 +0200

    hash-table-not-equal-test

--- lib/ltest/lpred.lisp
+++ lib/ltest/lpred.lisp
@@ -22,7 +22,7 @@ Custom predicates
 (defun not-equalp-p (v1 v2)
     (not (equalp v1 v2)))
 
- (defun hash-table-equal-p (t-1 t-2 &optional (predicate #'equal))
+(defun hash-table-equal-p (t-1 t-2 &optional (predicate #'equal))
     "Test equality between two hash tables by testing if they have the same
         size and if the keys of table one when used on table 2 refer to values
         that evaluate to true with the predicate equal (default predicate, can be changed)."
@@ -31,5 +31,10 @@ Custom predicates
             (and (gethash key t-1)
                 (funcall predicate (gethash key t-1) (gethash key t-2))))
         nil))
+
+(defun hash-table-not-equal-p (t1 t2 &optional (predicate #'equal))
+    "Tests non equality between two hash tables by the same principals as
+        hash-table-equal-p."
+    (not (hash-table-equal-p t1 t2 predicate)))
 
 
--- test/test-lpred.lisp
+++ test/test-lpred.lisp
@@ -9,6 +9,7 @@
         :name "Test custom predicates"
         :tests (list
             (test-hash-table-equal-p)
+            (test-hash-table-equal-p)
         )
     ))
 
@@ -47,4 +48,38 @@
                 (ltest:assertion
                     :val (lpred:hash-table-equal-p hash-table-1 hash-table-5)
                     :exp nil)))))
+
+(defun test-hash-table-not-equal-p ()
+    (let ((hash-table-1 (make-hash-table))
+            (hash-table-2 (make-hash-table))
+            (hash-table-3 (make-hash-table))
+            (hash-table-4 (make-hash-table))
+            (hash-table-5 (make-hash-table)))
+        (setf (gethash :value-1 hash-table-1) 1)
+        (setf (gethash :value-2 hash-table-1) :an-atom)
+        (setf (gethash :value-3 hash-table-1) "A string")
+        (setf (gethash :value-1 hash-table-2) 1)
+        (setf (gethash :value-2 hash-table-2) :an-atom)
+        (setf (gethash :value-3 hash-table-2) "A string")
+        (setf (gethash :value-1 hash-table-3) :an-atom)
+        (setf (gethash :value-2 hash-table-3) "A string")
+        (setf (gethash :value-3 hash-table-3) 1)
+        (setf (gethash :value-1 hash-table-4) 2)
+        (setf (gethash :value-2 hash-table-4) :an-other-atom)
+        (setf (gethash :value-3 hash-table-4) "An other string")
+        (setf (gethash :value-a hash-table-5) 1)
+        (setf (gethash :value-b hash-table-5) :an-atom)
+        (setf (gethash :value-c hash-table-5) "A string")
+        (ltest:test
+            :name "Test hash-table-not-equal-p"
+            :assertions (list 
+                (ltest:assertion
+                    :val (lpred:hash-table-equal-p hash-table-1 hash-table-2)
+                    :exp nil)
+                (ltest:assertion
+                    :val (lpred:hash-table-equal-p hash-table-1 hash-table-3))
+                (ltest:assertion
+                    :val (lpred:hash-table-equal-p hash-table-1 hash-table-4))
+                (ltest:assertion
+                    :val (lpred:hash-table-equal-p hash-table-1 hash-table-5))))))
     
